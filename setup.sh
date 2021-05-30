4d270c3fd5faeecb67244b1e418b3b42cb3d3760
RScottLewis <r.scott.lewis@gmail.com>
2021-05-30 15:10:46 -0400

    Initial commit my-app docker

---
+++ setup.sh
@@ -0,0 +1,16 @@
+ 1030  docker build -f Dockerfile-prod -t my-app:prod .
+ 1031  docker run -itd -p 80:80 --rm my-app:prod
+ 1032  echo mkdir .github && cd .github && mkdir workflows && cd workflows && touch deploy.yml\n
+ 1033  mkdir .github && cd .github && mkdir workflows && cd workflows && touch deploy.yml\n
+ 1034  vi deploy.yml
+ 1035  vi Do*
+ 1036  pwd
+ 1037  pwd
+ 1038  vi ../../Dockerfile
+ 1039  vi deploy.yml
+ 1040  vi deploy.yml
+ 1041  cd -
+ 1042  cd ..
+ 1043  vi Dockerfile-prod
+ 1044  history>setup.sh
+ 1045  vi setup.sh
